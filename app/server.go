141097fd0de77453f2c5653854585154e3a0353c
ttrang87 <trangta0807@gmail.com>
2025-01-06 13:56:22 -0500

    Response with body

--- app/server.go
+++ app/server.go
@@ -32,11 +32,28 @@ func main() {
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
 	req := make([]byte, 1024)
-	conn.Read(req)
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
-		return
+	n, err := conn.Read(req) //n is the number of bytes need
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	message := string(req[:n])
+	path := strings.Split(message, " ")[1] // split by space and extract second elenment (index 1)
+	response := ""
+
+	if path == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else if strings.HasPrefix(path, "/echo") {
+		text := path[6:]                                                                                                       // from req, "/echo/abc" => text = abc (from index 6)
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(text), text) // khớp value sau vào những ký hiệu %, &d for interger, %s for string and %f for float. Trả về string
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error sending response to client", err.Error())
+		os.Exit(1)
+	}
+
 }
