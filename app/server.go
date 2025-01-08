d695624b483451ea155ba5ef21ccf98642f0b160
ttrang87 <trangta0807@gmail.com>
2025-01-08 12:01:37 -0500

    Request a file and response  file

--- app/server.go
+++ app/server.go
@@ -61,6 +61,15 @@ func handleRequest(conn net.Conn) {
 			}
 		}
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+	} else if strings.HasPrefix(path, "/files/") {
+		fileName := strings.TrimPrefix(path, "/files/")
+		dir := os.Args[2]
+		data, err := os.ReadFile(dir + fileName)
+		if err != nil {
+			response = "HTTP/1.1 404 Not Found\r\n\r\n"
+		} else {
+			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
+		}
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
